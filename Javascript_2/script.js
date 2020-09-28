28e31ea77aae555300703bcb96a23bbb3b9ccf16
KylevanderWolf <68204507+KylevanderWolf@users.noreply.github.com>
2020-09-28 14:59:06 +0200

    Javascript-Node-Terminal

---
+++ Javascript_2/script.js
@@ -0,0 +1,18 @@
+
+//multiply (Dit is een grote som", wel zichtbaar in terminal)
+let multiply = (1230941 * 1823794)
+
+//Division (wel zichtbaar in terminal)
+let division = (637263 / 54)
+
+/* 
+Deze sommen zijn niet zichtbaar in de terminal (blocked out)
+let multiply = (1230941 * 1823794)
+
+//Division
+let division = (637263 / 54)
+*/
+
+console.log("Hallo Winc Academy studenten");
+console.log("Multiply:", multiply);
+console.log("Division:", division);
